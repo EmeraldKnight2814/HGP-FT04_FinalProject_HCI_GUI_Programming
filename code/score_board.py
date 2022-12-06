4847a20b6063aa31af29a5a8cb9989e9ffc18833
CollinRuggeberg <collin.ruggeberg1@marist.edu>
2022-12-06 12:53:46 +0000

    Update Rules and Buttons

--- code/score_board.py
+++ code/score_board.py
@@ -21,8 +21,10 @@ class ScoreBoard(QDockWidget):
         self.pass_button = QPushButton("Pass")
         self.pass_button.pressed.connect(self.passTurn)
         self.start_button = QPushButton("Start")
+        self.start_button.pressed.connect(self.startGame)
         self.reset_button = QPushButton("Reset")
         self.see_rules = QPushButton("See Rules")
+        self.see_rules.pressed.connect(self.seeRule)
 
         #create two labels which will be updated by signals
         self.label_clickLocation = QLabel("Click Location: ")
@@ -63,3 +65,28 @@ class ScoreBoard(QDockWidget):
 
     def passTurn(self):
         print("pass")
+
+    def startGame(self):
+            # TODO: Write start sequence
+
+    def seeRule(self):
+        ruleBox= QMessageBox(self)
+        ruleBox.setText("How to play/rules: Go is played by two players, called Black and White. The "
+                        "lines of the board have intersections wherever they cross or touch each other. "
+                        "Each intersection is called a point. Intersections at the four corners and the edges of the "
+                        "board are also called a point. Go is played on the points of the board, not on the squares. The "
+                        "points on which any stone is put are called occupied. All other points are called "
+                        "unoccupied or empty. Players take alternate turns. The player having the turn puts one of "
+                        "their stones on an empty point. Sometimes, to complete a move, a player removes stones from "
+                        "the board. Sometimes there are points that may not be played on a particular turn. A player may"
+                        " also pass instead of playing a stone on their turn. Once a stone is placed on the board, "
+                        "the stone does not move unless it is captured. A game of Go starts with an empty board and "
+                        "Black plays first, unless playing with handicap. The capture rule: If a player surrounds an "
+                        "opposing stone or stones by playing on all adjacent points, those opposing stone(s) "
+                        "are captured and are removed from the board. Every stone or string of stones must have at "
+                        "least one adjacent point that is unoccupied. This unoccupied point is called a liberty. "
+                        "Adjacent stones in a string share liberties, and the stones are said to have liberties. "
+                        "If there are no empty points next to a stone or a string of stones (i.e. no liberties), "
+                        "the stones are removed from the board. The only exception is that a capturing stone may "
+                        "have no liberty until the stones it captures are removed. ")
+        ruleBox.exec()
