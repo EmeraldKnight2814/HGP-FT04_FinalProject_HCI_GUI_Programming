de0c7b1a8d22e5ed9074999c09df4c4d6ffaec26
Daniel J Crowley <dcrow2814@gmail.com>
2022-12-06 13:15:55 +0000

    adjusting board to make it look better

--- code/board.py
+++ code/board.py
@@ -8,8 +8,8 @@ class Board(QFrame):  # base the board on a QFrame widget
     updateTimerSignal = pyqtSignal(int) # signal sent when timer is updated
     clickLocationSignal = pyqtSignal(str) # signal sent when there is a new click location
 
-    boardWidth  = 8     # board is 7 squares wide
-    boardHeight = 8     # board is 7 squares tall
+    boardWidth  = 8     # board is 8 squares wide
+    boardHeight = 8     # board is 8 squares tall
     timerSpeed  = 1000     # the timer updates every 1 second
     counter     = 100    # the number the counter will count down from
 
@@ -22,6 +22,7 @@ class Board(QFrame):  # base the board on a QFrame widget
         self.timer = QBasicTimer()  # create a timer for the game
         self.isStarted = False      # game is not currently started
         self.start()                # start the game which will start the timer
+        self.setStyleSheet("background-color: black;")
 
         self.boardArray =[[0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 4, 5, 6], [0, 1, 2, 3, 4, 5, 6]]        # TODO - create a 2d int/Piece array to store the state of the game
         self.printBoardArray()
@@ -85,10 +86,7 @@ class Board(QFrame):  # base the board on a QFrame widget
 
     def drawBoardSquares(self, painter):
         '''draw all the square on the board'''
-        # TODO set the default colour of the brush
         self.brush = QBrush(Qt.BrushStyle.SolidPattern)
-        self.brush.setColor(QColor(0, 0, 0))
-        painter.setBrush(self.brush)
         for row in range(0, Board.boardHeight):
             for col in range (0, Board.boardWidth):
                 painter.save()
@@ -115,7 +113,21 @@ class Board(QFrame):  # base the board on a QFrame widget
                             self.brush.setColor(QColor(138, 109, 49))
                             painter.setBrush(self.brush)
 
-                painter.fillRect(col, row, self.squareWidth(), self.squareHeight(), painter.brush())
+                if(row == 0 and col == 0):
+                    painter.fillRect(col, row, self.squareWidth() + 2, self.squareHeight() - 2, painter.brush())
+                elif(row == 7 and col == 0):
+                    painter.fillRect(col, row, self.squareWidth() + 2, self.squareHeight() + 2, painter.brush())
+                elif (row == 0 and col == 7):
+                    painter.fillRect(col, row, self.squareWidth() + 2, self.squareHeight() + 2, painter.brush())
+                elif (row == 7 and col == 7):
+                    painter.fillRect(col, row, self.squareWidth() + 2, self.squareHeight() + 2, painter.brush())
+                elif(row == 0 or row == 7):
+                    painter.fillRect(col, row, self.squareWidth() + 2, self.squareHeight() - 2, painter.brush())
+                elif(col == 0 or col == 7):
+                    painter.fillRect(col, row, self.squareWidth() - 2, self.squareHeight() + 2, painter.brush())
+                else:
+                    painter.fillRect(col, row, self.squareWidth() - 2, self.squareHeight() - 2, painter.brush())
+
                 painter.restore()
 
     def drawPieces(self, painter):
@@ -124,20 +136,16 @@ class Board(QFrame):  # base the board on a QFrame widget
         brush = QBrush(Qt.BrushStyle.SolidPattern)
         brush.setColor(colour)
 
-        #TEST BRUSH TO GET SIZE AND SHAPE RIGHT
-        color = Qt.GlobalColor.blue
-        testBrush = QBrush(Qt.BrushStyle.SolidPattern)
-        testBrush.setColor(color)
-
         for row in range(0, len(self.boardArray)):
             for col in range(0, len(self.boardArray[0])):
                 painter.save()
+                # set transformation so that each piece is on an intersection
                 colTransformation = self.squareWidth() * col + (self.squareWidth() * .75)
                 rowTransformation = self.squareHeight() * row + (self.squareHeight() * .75)
                 painter.translate(colTransformation, rowTransformation)
-                # TODO choose your colour and set the painter brush to the correct colour
-                painter.setBrush(testBrush)         #For testing purposes
-                #painter.setBrush(brush)            #Actual brush
+                # set color
+                painter.setBrush(brush)
+                # set piece size & draw
                 radius = self.squareWidth() / 4
                 center = QPointF(radius, radius)
                 painter.drawEllipse(center, radius, radius)
